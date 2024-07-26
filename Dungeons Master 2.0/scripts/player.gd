f278e074689fd563938ff4b503169786b8f57a28
Milan <mujdar.milan@gmail.com>
2024-07-27 00:13:24 +0300

    removed bad code and updated animations for left right and also for holding the sword -> safezone or not

--- Dungeons Master 2.0/scripts/player.gd
+++ Dungeons Master 2.0/scripts/player.gd
@@ -3,20 +3,35 @@ extends CharacterBody2D
 const WALL_ZIndex = -14
 
 @export var movement_speed : float = 100
-@onready var sprite = $Sprite2D
-@onready var animation_player = $AnimationPlayer
-@onready var animation_tree = $AnimationTree
-@onready var layer_detection = $LayerDetection
+@onready var sprite : Sprite2D = $Sprite2D
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var animation_tree : AnimationTree = $AnimationTree
+@onready var layer_detection : Area2D = $LayerDetection
+@onready var sword : Sprite2D = $Sword
+
+
+@onready var hand1 : Sprite2D = sprite.find_child("Hand1")
+@onready var hand2 : Sprite2D = sprite.find_child("Hand2")
+@onready var body : Sprite2D = sprite.find_child("Body")
+
+@export var safezone : bool = true
 
 var direction : Vector2 = Vector2.ZERO
+var facing_direction : int = -1 # 1 for right , -1 for left
 
 func _ready():
 	animation_tree.active = true
+	# animation_tree["parameters/conditions/in_safezone"] = true
 	sprite.z_index = 0
-	# animation_player.play("Idle")
-	face_right()
 
 func update_animation_parameters():
+	if safezone == true:	
+		animation_tree["parameters/conditions/in_safezone"] = true
+		animation_tree["parameters/conditions/in_dangerzone"] = false
+	else:
+		animation_tree["parameters/conditions/in_safezone"] = false
+		animation_tree["parameters/conditions/in_dangerzone"] = true
+	
 	if velocity == Vector2.ZERO:
 		animation_tree["parameters/conditions/idle"] = true
 		animation_tree["parameters/conditions/is_moving"] = false
@@ -25,21 +40,14 @@ func update_animation_parameters():
 		animation_tree["parameters/conditions/idle"] = false
 	
 	# If adding different animations for up down left right , careful here !
-	# animation_tree["parameters/Idle/blend_position"] = direction
-	# animation_tree["parameters/Running/blend_position"] = direction
+	if direction != Vector2.ZERO:
+		animation_tree["parameters/Idle/blend_position"] = direction - Vector2(-facing_direction, 0)
+		animation_tree["parameters/Idle_with_weapon/blend_position"] = direction - Vector2(-facing_direction, 0)
+		animation_tree["parameters/Running/blend_position"] = direction - Vector2(-facing_direction, 0)
+		#print(direction - Vector2(-facing_direction, 0))
 	
 func _process(delta):
 	pass
-
-func face_left():
-	sprite.find_child("Body").flip_h = true
-	sprite.find_child("Hand1").z_index = sprite.find_child("Body").z_index - 2
-	sprite.find_child("Hand2").z_index = sprite.find_child("Body").z_index
-
-func face_right():
-	sprite.find_child("Body").flip_h = false
-	sprite.find_child("Hand1").z_index = sprite.find_child("Body").z_index
-	sprite.find_child("Hand2").z_index = sprite.find_child("Body").z_index - 2
 
 func _physics_process(delta):
 	# Get the input direction
@@ -57,13 +65,9 @@ func _physics_process(delta):
 	velocity = input_direction * movement_speed * movement_scaling_factor
 	
 	# Update facing direction
+	if input_direction.x != 0:
+		facing_direction = input_direction.x
 	
-	var facing_direction = input_direction.x
-	if facing_direction == -1:
-		face_left()
-	elif facing_direction == 1:
-		face_right()
-		
 	# Play the correct animation
 	update_animation_parameters()
 	
